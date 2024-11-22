a8022542ad1a0a378ade0e19d663b708b934b6aa
suggus7 <ad.delgado@hotmail.com>
2024-11-22 04:02:07 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let carouselIndex = 0;
+const carouselItems = document.querySelectorAll('.carousel-item');
+
+function showNextCarouselItem() {
+  carouselItems[carouselIndex].classList.remove('active');
+  carouselIndex = (carouselIndex + 1) % carouselItems.length;
+  carouselItems[carouselIndex].classList.add('active');
+}
+
+setInterval(showNextCarouselItem, 2000);
